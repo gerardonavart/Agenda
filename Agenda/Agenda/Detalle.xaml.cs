183c48018114d8a10a7cdc054aab732269d432ff
Gerardo Nava <gerardonavart@outlook.com>
2018-06-06 00:34:08 -0500

    Agregar archivos de proyecto.

---
+++ Agenda/Agenda/Detalle.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Agenda
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Detalle : ContentPage
+	{
+        List<Amigo> listaAmigos;
+		public Detalle (List<Amigo> amigos)
+		{
+			InitializeComponent ();
+            btnGuardar.Clicked += BtnGuardar_Clicked;
+            listaAmigos = amigos;
+		}
+
+        private void BtnGuardar_Clicked(object sender, EventArgs e)
+        {
+            listaAmigos.Add(new Amigo()
+            { Apodo = txbApodo.Text, Email = txbEmail.Text, Nombre = txbNombre.Text, Telefono = txbTelefono.Text });
+            Navigation.PopAsync();
+        }
+    }
+}
---
+++ Agenda/Agenda/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Agenda
+{
+	public partial class MainPage : ContentPage
+	{
+        List<Amigo> amigos;
+		public MainPage()
+		{
+			InitializeComponent();
+            amigos = new List<Amigo>();
+            btnAgregar.Clicked += BtnAgregar_Clicked;
+		}
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            lstAgenda.ItemsSource = null;
+            lstAgenda.ItemsSource = amigos;
+        }
+
+        private void BtnAgregar_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Detalle(amigos));
+        }
+    }
+}
